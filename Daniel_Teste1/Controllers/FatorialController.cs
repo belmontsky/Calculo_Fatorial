0f71dfa4ee08fdd61c2b3cb211416e4b4fc03b24
belmontsky <ferkusanagi@gmail.com>
2023-09-18 10:38:59 -0300

    Adicionar arquivos de projeto.

---
+++ Daniel_Teste1/Controllers/FatorialController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Daniel_Teste1.Models;
+
+namespace Daniel_Teste1.Controllers
+{
+    public class FatorialController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new FatorialModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(FatorialModel model)
+        {
+            if (model.Numero < 0)
+            {
+                ModelState.AddModelError("Numero", "O número precisa ser não negativo.");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Daniel_Teste1/Models/FatorialModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Daniel_Teste1.Models
+{
+    public class FatorialModel
+    {
+        public int Numero { get; set; }
+        public long Resultado => CalcularFatorial(Numero);
+
+        private long CalcularFatorial(int n)
+        {
+            if (n == 0) return 1;
+            long fatorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fatorial *= i;
+            }
+            return fatorial;
+        }
+    }
+}
+
